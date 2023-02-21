76d61f9ed039bd66effb1f0059be6cbebf9d6af3
NoranLearner <noranmostafa843@gmail.com>
2023-02-22 01:43:57 +0200

    SignUp & Login Validate

--- src/components/Login/LoginForm.vue
+++ src/components/Login/LoginForm.vue
@@ -1,51 +1,90 @@
 <template>
     <form @click.prevent>
-            <h1>Login</h1>
-            <div class="row g-3 align-items-center">
-                <div class="col-auto d-block mx-auto">
-                    <input type="email" class="form-control" placeholder="Enter Your Email" v-model="email">
-                </div>
+        <h1>Login</h1>
+        <div class="row g-3 align-items-center">
+            <div class="col-auto d-block mx-auto">
+                <input type="email" class="form-control" placeholder="Enter Your Email" v-model="state.email" />
+                <span class="error-feedback" v-if="v$.email.$error">{{
+                    v$.email.$errors[0].$message
+                }}</span>
             </div>
-            <br />
-            <div class="row g-3 align-items-center">
-                <div class="col-auto d-block mx-auto">
-                    <input type="password" class="form-control" placeholder="Enter Your Password" v-model="password">
-                </div>
+        </div>
+        <br />
+        <div class="row g-3 align-items-center">
+            <div class="col-auto d-block mx-auto">
+                <input type="password" class="form-control" placeholder="Enter Your Password" v-model="state.password" />
+                <span class="error-feedback" v-if="v$.password.$error">{{
+                    v$.password.$errors[0].$message
+                }}</span>
             </div>
-            <br />
-            <div class="row g-3 align-items-center">
-                <div class="col-auto d-block mx-auto">
-                    <button type="submit" class="btn btn-secondary">Login Now</button>
-                    &nbsp;
-                    <!-- <button type="button" class="btn btn-link" @click="signupPage()">Sign Up</button> -->
-                    <button type="button" class="btn btn-link" @click="redirectTo({val: 'sign-up'})">Sign Up</button>
-                </div>
+        </div>
+        <br />
+        <div class="row g-3 align-items-center">
+            <div class="col-auto d-block mx-auto">
+                <button type="submit" class="btn btn-secondary" @click="loginUser()">
+                    Login Now
+                </button>
+                &nbsp;
+                <!-- <button type="button" class="btn btn-link" @click="signupPage()">Sign Up</button> -->
+                <button type="button" class="btn btn-link" @click="redirectTo({ val: 'sign-up' })">
+                    Sign Up
+                </button>
             </div>
-        </form>
+        </div>
+    </form>
 </template>
 
 <script>
-
-import {mapActions} from "vuex";
-
+import { mapActions } from "vuex";
+import useValidate from "@vuelidate/core";
+import { required, email } from "@vuelidate/validators";
+// For Composition Api
+import { reactive, computed } from "vue";
 export default {
     name: "LoginForm",
-    data() {
-        return {
+    // Composition API
+    setup() {
+        // Data
+        const state = reactive({
             email: "",
             password: "",
-        }
+        });
+        // Validations
+        const rules = computed(() => {
+            return {
+                email: { required, email },
+                password: { required },
+            };
+        });
+        // Write rules first
+        const v$ = useValidate(rules, state);
+        return { state, v$ };
+    },
+    data() {
+        return {
+        };
     },
     methods: {
         /* signupPage(){
-            // Use name in router file
-            this.$router.push({name:'sign-up'});
-        }, */
-        ...mapActions(['redirectTo'])
+                // Use name in router file
+                this.$router.push({name:'sign-up'});
+            }, */
+        ...mapActions(["redirectTo"]),
+        loginUser() {
+            this.v$.$validate();
+            if (!this.v$.$error) {
+                console.log("Form Validated Successfully");
+            } else {
+                console.log("Form Validation Failed");
+            }
+        },
     },
-}
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.error-feedback {
+    color: red;
+    font-size: 0.85em;
+}
 </style>
--- src/components/SignUp/SignUpForm.vue
+++ src/components/SignUp/SignUpForm.vue
@@ -4,24 +4,27 @@
         <div class="row g-3 align-items-center">
             <div class="col-auto d-block mx-auto">
                 <input type="text" class="form-control" placeholder="Enter Your Name" v-model="name">
+                <span class="error-feedback" v-if="v$.name.$error">{{ v$.name.$errors[0].$message }}</span>
             </div>
         </div>
         <br />
         <div class="row g-3 align-items-center">
             <div class="col-auto d-block mx-auto">
                 <input type="email" class="form-control" placeholder="Enter Your Email" v-model="email">
+                <span class="error-feedback" v-if="v$.email.$error">{{ v$.email.$errors[0].$message }}</span>
             </div>
         </div>
         <br />
         <div class="row g-3 align-items-center">
             <div class="col-auto d-block mx-auto">
                 <input type="password" class="form-control" placeholder="Enter Your Password" v-model="password">
+                <span class="error-feedback" v-if="v$.password.$error">{{ v$.password.$errors[0].$message }}</span>
             </div>
         </div>
         <br />
         <div class="row g-3 align-items-center">
             <div class="col-auto d-block mx-auto">
-                <button type="submit" class="btn btn-secondary">Sign Up Now</button>
+                <button type="submit" class="btn btn-secondary" @click="signUpNow()">Sign Up Now</button>
                 &nbsp;
                 <!-- <button type="button" class="btn btn-link" @click="loginPage()">Login</button> -->
                 <button type="button" class="btn btn-link" @click="redirectTo({val: 'login'})">Login</button>
@@ -32,13 +35,23 @@
 
 <script>
 import { mapActions } from 'vuex';
+import useValidate from "@vuelidate/core";
+import { required, email, minLength } from "@vuelidate/validators";
 export default {
     name: "SignUpForm",
     data() {
         return {
+            v$: useValidate(),
             name: "",
             email: "",
             password:"",
+        }
+    },
+    validations() {
+        return {
+            name: {required, minLength: minLength(10)},
+            email: {required, email},
+            password: { required, minLength: minLength(10) },
         }
     },
     methods: {
@@ -47,8 +60,21 @@ export default {
             this.$router.push({name:'login'});
         }, */
         ...mapActions(['redirectTo']),
+        signUpNow() {
+            this.v$.$validate();
+            if (!this.v$.$error) {
+                console.log('Form Validated Successfully');
+            } else {
+                console.log('Form Validation Failed');
+            }
+        },
     },
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.error-feedback{
+    color: red;
+    font-size: 0.85em;
+    }
+</style>
